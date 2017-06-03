5011a6a0f19141d7d307c9a39cddf061a3edef8d
Barns <bcar289@aucklanduni.ac.nz>
2017-06-03 12:52:29 +1200

    Created the BlobManipulator class.

--- src/User/User.java
+++ src/User/User.java
@@ -6,9 +6,6 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
-/**
- * Created by Barns on 3/06/17.
- */
 public class User implements Serializable {
 
     private String username;
---
+++ src/Utility/BlobManipulator.java
@@ -0,0 +1,39 @@
+package Utility;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.sql.Blob;
+import java.sql.SQLException;
+
+/**
+ * Created by Barns on 31/05/17.
+ */
+public class BlobManipulator {
+
+    public static byte[] getByteArray(Blob blob) {
+        byte[] bytes;
+
+        try {
+            SerialBlob serialBlob = new SerialBlob(blob);
+            bytes = serialBlob.getBytes(1, (int) blob.length());
+            return bytes;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Blob getBlob(byte[] bytes) {
+        Blob blob = null;
+
+        try {
+            blob = new SerialBlob(bytes);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return blob;
+    }
+
+
+
+}
