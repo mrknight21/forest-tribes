e18a29d3954828d3fb1d2e431be7bfa8843cdcd3
mche618 <mche618@aucklanduni.ac.nz>
2017-06-07 21:04:03 +1200

    Delete function completed

--- src/User/SQL_Statements.sql
+++ src/User/SQL_Statements.sql
@@ -5,18 +5,19 @@
 
 -- Statements to create the table which will store the User information.
 DROP TABLE IF EXISTS inFoJaxs_User;
+
 CREATE TABLE IF NOT EXISTS inFoJaxs_User (
   username VARCHAR(50) NOT NULL UNIQUE,
   first_name VARCHAR(25) NOT NULL,
   last_name VARCHAR(25) NOT NULL,
   email VARCHAR(50) NOT NULL,
-  ProfileImagePath VARCHAR (400) NOT NULL,
-  UserFolderPath VARCHAR (400) NOT NULL,
+  date_of_birth DATE,
   PRIMARY KEY (username)
 );
 
 -- Statements to create the table which will the UserSecurity information.
 DROP TABLE IF EXISTS inFoJaxs_UserSecurity;
+
 CREATE TABLE IF NOT EXISTS inFoJaxs_UserSecurity (
   userID INT AUTO_INCREMENT,
   username VARCHAR(50),
@@ -24,7 +25,7 @@ CREATE TABLE IF NOT EXISTS inFoJaxs_UserSecurity (
   iterations INT NOT NULL,
   hash BLOB NOT NULL,
   PRIMARY KEY (userID),
-  FOREIGN KEY (username) REFERENCES inFoJaxs_User(username) ON DELETE CASCADE
+  FOREIGN KEY (username) REFERENCES inFoJaxs_User(username)
 );
 
 
@@ -40,7 +41,7 @@ CREATE TABLE IF NOT EXISTS inFoJaxs_Profile (
   short_intro TEXT,
   CHECK (date_Of_Birth Like '__/__/____' ),
   PRIMARY KEY (username),
-  FOREIGN KEY (username) REFERENCES inFoJaxs_User(username) ON DELETE CASCADE
+  FOREIGN KEY (username) REFERENCES inFoJaxs_User(username)
 );
 
 DROP TABLE IF EXISTS inFoJaxs_IssuesCared;
@@ -49,7 +50,7 @@ CREATE TABLE IF NOT EXISTS inFoJaxs_IssuesCared (
   username VARCHAR(50) NOT NULL,
   issues_Cared VARCHAR (50) NOT NULL ,
   PRIMARY KEY (ID),
-  FOREIGN KEY (username) REFERENCES inFoJaxs_User(username) ON DELETE CASCADE
+  FOREIGN KEY (username) REFERENCES inFoJaxs_User(username)
 );
 
 ALTER TABLE inFoJaxs_User
@@ -57,8 +58,11 @@ ADD UserFolderPath VARCHAR (400) NULL ;
 Update inFoJaxs_User set ProfileImagePath = NULL
 
 
-
-
+DROP TABLE IF EXISTS inFoJaxs_ReplyLikes;
+DROP TABLE IF EXISTS inFoJaxs_CommentLikes;
+DROP TABLE IF EXISTS inFoJaxs_ArticleLikes;
+DROP TABLE IF EXISTS inFoJaxs_Replies;
+DROP TABLE IF EXISTS inFoJaxs_Comments;
 DROP TABLE IF EXISTS inFoJaxs_Articles;
 CREATE TABLE IF NOT EXISTS inFoJaxs_Articles (
   ID INT AUTO_INCREMENT,
@@ -68,55 +72,50 @@ CREATE TABLE IF NOT EXISTS inFoJaxs_Articles (
   creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   lastEdit TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (ID),
-  FOREIGN KEY (username) REFERENCES inFoJaxs_User(username) ON DELETE CASCADE
+  FOREIGN KEY (username) REFERENCES inFoJaxs_User(username)
 );
 
-DROP TABLE IF EXISTS inFoJaxs_Comments;
 CREATE TABLE IF NOT EXISTS inFoJaxs_Comments (
   ID INT AUTO_INCREMENT,
-  article_ID INT NOT NULL,
+  parent_ID INT NOT NULL,
   username VARCHAR(50) NOT NULL ,
   content VARCHAR (8000) NOT NULL,
   creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   lastEdit TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (ID),
-  FOREIGN KEY (username) REFERENCES inFoJaxs_User(username) ON DELETE CASCADE,
-  FOREIGN KEY (article_ID) REFERENCES inFoJaxs_Articles(ID) ON DELETE CASCADE
+  FOREIGN KEY (username) REFERENCES inFoJaxs_User(username),
+  FOREIGN KEY (parent_ID) REFERENCES inFoJaxs_Articles(ID)
 );
 
-DROP TABLE IF EXISTS inFoJaxs_Replies;
 CREATE TABLE IF NOT EXISTS inFoJaxs_Replies (
   ID INT AUTO_INCREMENT,
-  comment_ID INT NOT NULL ,
+  parent_ID INT NOT NULL ,
   username VARCHAR(50) NOT NULL,
   content VARCHAR (8000) NOT NULL,
   creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   lastEdit TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (ID),
-  FOREIGN KEY (username) REFERENCES inFoJaxs_User(username) ON DELETE CASCADE,
-  FOREIGN KEY (comment_ID) REFERENCES inFoJaxs_Comments(ID) ON DELETE CASCADE
+  FOREIGN KEY (username) REFERENCES inFoJaxs_User(username),
+  FOREIGN KEY (parent_ID) REFERENCES inFoJaxs_Comments(ID)
 );
 
-DROP TABLE IF EXISTS inFoJaxs_ArticleLikes;
 CREATE TABLE IF NOT EXISTS inFoJaxs_ArticleLikes (
   ID INT,
   likes INT DEFAULT 0,
   PRIMARY KEY (ID),
-  FOREIGN KEY (ID) REFERENCES inFoJaxs_Articles(ID) ON DELETE CASCADE
+  FOREIGN KEY (ID) REFERENCES inFoJaxs_Articles(ID)
 );
 
-DROP TABLE IF EXISTS inFoJaxs_CommentLikes;
 CREATE TABLE IF NOT EXISTS inFoJaxs_CommentLikes (
   ID INT,
   likes INT DEFAULT 0,
   PRIMARY KEY (ID),
-  FOREIGN KEY (ID) REFERENCES inFoJaxs_Comments(ID) ON DELETE CASCADE
+  FOREIGN KEY (ID) REFERENCES inFoJaxs_Comments(ID)
 );
 
-DROP TABLE IF EXISTS inFoJaxs_ReplyLikes;
 CREATE TABLE IF NOT EXISTS inFoJaxs_ReplyLikes (
   ID INT,
   likes INT DEFAULT 0,
   PRIMARY KEY (ID),
-  FOREIGN KEY (ID) REFERENCES inFoJaxs_Replies(ID) ON DELETE CASCADE
+  FOREIGN KEY (ID) REFERENCES inFoJaxs_Replies(ID)
 );
